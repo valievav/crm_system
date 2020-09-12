3eecb3a58e5e6b5ce935aba8c1c55d8af8afbd0c
Venus <valievavm@gmail.com>
2020-09-12 23:31:37 +0200

    Add models, migration, register models in admin

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Product, Order, Tag
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'date_created')
+    list_display_links = ('id', 'name')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'product', 'status', 'date_created')
+    list_display_links = ('id', 'customer', 'product')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date_created')
+    list_display_links = ('id', 'name')
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY, max_length=100)
+    description = models.TextField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(choices=STATUS, max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
